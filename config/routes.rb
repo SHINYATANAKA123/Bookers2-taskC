1d198ff155f2f9b154d51cf3c213c0c8c2209a00
SHINYATANAKA123 <123asde.med@gmail.com>
2021-06-09 08:38:21 +0000

    [Add] mail機能

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update]
   resources :groups do
     get "join" => "groups#join"
+    get "new/mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
   end
 end
